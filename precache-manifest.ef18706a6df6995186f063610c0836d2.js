8ec51f1822686925c57723cf82036e6c3e0faeb4
Lalu Arman Rozika <armanrozika@gmail.com>
2019-04-08 11:19:22 +0700

    Add files via upload

---
+++ precache-manifest.ef18706a6df6995186f063610c0836d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5f1bf43a75019ce2cee",
+    "url": "/static/js/main.3e15ee77.chunk.js"
+  },
+  {
+    "revision": "cc3a929f0aa7004d1955",
+    "url": "/static/js/2.8b42144f.chunk.js"
+  },
+  {
+    "revision": "a5f1bf43a75019ce2cee",
+    "url": "/static/css/main.af8653c7.chunk.css"
+  },
+  {
+    "revision": "f6b3b0478d1afbc1ced217e6aab5f6c9",
+    "url": "/index.html"
+  }
+];
